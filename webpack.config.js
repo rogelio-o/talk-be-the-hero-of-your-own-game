9dee6b8ced950fe38d05b9cd7907b9836fd8f290
rogelio-o <yo@rogelioorts.com>
2019-09-28 17:28:09 +0200

    Update configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/app/index.ts",
   plugins: [
     new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ["public/build"]
+      cleanAfterEveryBuildPatterns: ["public/build"],
     }),
     new HtmlWebpackPlugin({
-      template: "src/templates/index.html"
-    })
+      template: "src/templates/index.html",
+    }),
+    new CopyWebpackPlugin([{ from: "static" }]),
   ],
   output: {
     path: __dirname + "/public",
-    filename: "build/[name].[contenthash].js"
+    filename: "build/[name].[contenthash].js",
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: [".ts", ".js"],
   },
   module: {
-    rules: [{ test: /\.ts?$/, loader: "ts-loader" }]
-  }
+    rules: [{ test: /\.ts?$/, loader: "ts-loader" }],
+  },
 };

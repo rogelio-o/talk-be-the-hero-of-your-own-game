cc9faf8239fee4129426bd3976c06fd1a8426640
rogelio-o <yo@rogelioorts.com>
2019-09-29 21:23:45 +0200

    Random movement of not controlled player

--- src/app/game/player/NotControlledPlayer.ts
+++ src/app/game/player/NotControlledPlayer.ts
@@ -1,12 +1,35 @@
-import { AbstractPlayer } from "./AbstractPlayer";
+import { AbstractPlayer, PlayerStatus } from "./AbstractPlayer";
 import { IPlayer } from "./IPlayer";
 
 export class NotControlledPlayer extends AbstractPlayer implements IPlayer {
+  private intervalId?: any;
+
   public startMoving(): void {
-    // Do nothing
+    this.intervalId = setInterval(() => {
+      const rand = Math.floor(Math.random() * 10);
+
+      if (rand < 4) {
+        this.changeStatus(PlayerStatus.WALKING_LEFT);
+      } else if (rand > 4) {
+        this.changeStatus(PlayerStatus.WALKING_RIGHT);
+      } else {
+        this.changeStatus(PlayerStatus.AWAITING);
+      }
+
+      if (rand < 3) {
+        this.changePunching(true);
+      } else {
+        this.changePunching(false);
+      }
+    }, 500);
   }
 
   public stopMoving(): void {
-    // Do nothing
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    this.changeStatus(PlayerStatus.AWAITING);
+    this.changeGettingAPunch(false);
+    this.changePunching(false);
   }
 }
